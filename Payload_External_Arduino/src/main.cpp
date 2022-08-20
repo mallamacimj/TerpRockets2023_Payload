2acaad586f9702e685f493eca775b6987324fff2
Michael Mallamaci <mallamacimj@gmail.com>
2022-08-20 18:48:47 -0400

    Created basic outline of what the main function will do

--- Payload_External_Arduino/src/main.cpp
+++ Payload_External_Arduino/src/main.cpp
@@ -1,11 +1,29 @@
 #include <Arduino.h>
 #include <external_arduino_functions.cpp>
-#include <SD.h>
+
+//global varibles for staging
+bool LAUNCH = 0; //0 if launch has not occured, 1 if it has
+bool DROGUE = 0; //0 if main drogue not deployed, 1 if it has
+bool SEPARTION = 0; //0 if not good for separation, 1 if it is
 
 void setup() {
   // put your setup code here, to run once:
+  SD_begin();
+  MPU6050_begin();
+  BH1750_begin();
+  Buzzer();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  LAUNCH = determineLaunch(LAUNCH);
+  startInnerArduino();
+  DROGUE = determineDrogue(DROGUE);
+  openDoor();
+  SEPARTION = determineSepartion(SEPARTION);
+  startParachuteTimer();
+  openStraps();
+  closeDoor();
+  
 }
