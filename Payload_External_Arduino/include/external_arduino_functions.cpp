229b5d4f98def4416002621d3edec7f5371007e2
Michael Mallamaci <mallamacimj@gmail.com>
2022-08-20 18:49:11 -0400

    Created the function and function prototypes for everything used in the main function

--- Payload_External_Arduino/include/external_arduino_functions.cpp
+++ Payload_External_Arduino/include/external_arduino_functions.cpp
@@ -1,3 +1,104 @@
+//Add libraries needed here
 #include "external_arduino_functions.hpp"
+#include <SPI.h>
+#include <SD.h>
+
+//Arduino Pins
+int buzzer_pin = 2;
+int door_pin = 4;
+int servo1_pin = 5;
+int servo2_pin = 6;
+int str_pin = 7;
+int servo3_pin = 9;
+int servo4_pin = 10;
+int sd_pin = 8;
+int servopwr_pin = A0;
+
+//Add const values here
+
 
 //add functions here
+void SD_begin(){
+  //Start the SD card module
+}
+
+void MPU6050_begin(){
+  //Start the accelerameter and gyroscope module
+}
+
+void BH1750_begin(){
+  //Start the light sensor module
+}
+
+void Buzzer(){
+  pinMode(buzzer_pin, OUTPUT);
+  digitalWrite(buzzer_pin, HIGH);
+  delay(1000);
+  digitalWrite(buzzer_pin, LOW);
+}
+
+bool determineLaunch(bool launch){
+  //This function must loop until launch occurs
+
+  if (launch == 1){
+    return launch = 1; //End the function if Launch has already occured
+  }
+  //Use the accelerameter to determine launch
+  //must also write to the SD with data before launch
+
+
+  return launch = 1;
+}
+
+void startInnerArduino(){
+  pinMode(str_pin, OUTPUT);
+  digitalWrite(str_pin, HIGH);
+}
+
+bool determineDrogue(bool drogue){
+  //This function must loop until drogue deployment occurs
+  if (drogue == 1){
+    return drogue = 1;
+  }
+  //Use the accelerameter to determine main drogue
+  //separation must also write to the SD with data
+  //before main drogue separation
+
+  return drogue = 1;
+}
+
+void openDoor(){
+  //Activiates servo 3 to open door
+  //This is different than the door pin
+  //Don't forget to turn on servo power
+}
+
+bool determineSepartion(bool separation){
+  //This function must loop until separation is good
+  if (separation == 1){
+    return separation = 1;
+  }
+  //Use the light sensor to determine when separation
+  //can occur also write to the SD with data
+  //about this separation
+
+  return separation = 1;
+}
+
+void startParachuteTimer(){
+  //Using I2C, connect with the inner payload
+  //arduino and tell it to start a countdown
+  //to release it's parachute
+}
+
+void openStraps(){
+  //Activiates servo 1 and 2 to release the
+  //straps holding the inner payload in place
+  //Don't forget to turn on servo power
+}
+
+void closeDoor(){
+  //Activiates servo 3 to close door
+  //This is different than the door pin
+  //Don't forget to turn on servo power
+}
--- Payload_External_Arduino/include/external_arduino_functions.hpp
+++ Payload_External_Arduino/include/external_arduino_functions.hpp
@@ -2,6 +2,17 @@
 #define EXTERNAL_ARDUINO_FUNCTIONS_HPP
 
 //Add function prototypes here
-
+void SD_begin();
+void MPU6050_begin();
+void BH1750_begin();
+void Buzzer();
+bool determineLaunch(bool);
+void startInnerArduino();
+bool determineDrogue(bool);
+void openDoor();
+bool determineSepartion(bool);
+void startParachuteTimer();
+void openStraps();
+void closeDoor();
 
 #endif
