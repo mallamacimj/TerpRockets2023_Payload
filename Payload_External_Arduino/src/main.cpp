4f8f1dd55068fa1a3adb07ba7ae4f0d940512a49
Michael Mallamaci <mallamacimj@gmail.com>
2022-08-24 15:57:23 -0400

    Update main.cpp

--- Payload_External_Arduino/src/main.cpp
+++ Payload_External_Arduino/src/main.cpp
@@ -2,6 +2,7 @@
 #include <external_arduino_functions.cpp>
 
 //global varibles for staging
+//Make ONE stage variable that has all this information
 bool LAUNCH = 0; //0 if launch has not occured, 1 if it has
 bool DROGUE = 0; //0 if main drogue not deployed, 1 if it has
 bool SEPARTION = 0; //0 if not good for separation, 1 if it is
@@ -17,6 +18,15 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //Here add the data gathering for each loop
+  //That means functions that output RTC, Accel, Gyro
+  //and maybe light sensor (only do that after launch)
+
+
+  //This code down here isn't going to work
+  //Like the global variables for stage (but make one)
+  //Need to change these functions so that they always loop
+  //This means adding some if statements but thats fine 
   LAUNCH = determineLaunch(LAUNCH);
   startInnerArduino();
   DROGUE = determineDrogue(DROGUE);
@@ -25,5 +35,5 @@ void loop() {
   startParachuteTimer();
   openStraps();
   closeDoor();
-  
+
 }
